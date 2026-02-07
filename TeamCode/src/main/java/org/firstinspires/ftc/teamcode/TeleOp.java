fef8ca61ff1d1b6bd6812e6abeb9a54ae8db8077
Nick-blip-308 <nickpradhan1212@gmail.com>
2026-02-06 19:39:55 -0800

    changing the power on flywheel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -111,7 +111,7 @@ public class TeleOp extends LinearOpMode {
 
         waitForStart();
         runtime.reset();
-        flyWheel.setPower(0.3);
+        flyWheel.setPower(0.5);
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
